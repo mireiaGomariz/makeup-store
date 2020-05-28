d2fb9e228a29d50f1defb9833a8872ba4d4c96f4
mireiaGomariz <mireiapgomariz@gmail.com>
2020-05-28 11:17:39 +0200

    navbar styles and links

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ab2f1725990aa8e31c4b";
+/******/ 	var hotCurrentHash = "1a328c63da17f3c3a7af";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
